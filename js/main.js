bb335fabab02cc0fd130ef8e21f277f41eee41c4
AlexNexton <cianoc13@gmail.com>
2020-06-19 12:31:52 +0000

    docs: video's of Consuming APIs Using JavaScript in CI course - callback function

--- js/main.js
+++ js/main.js
@@ -1,8 +1,36 @@
 //java Script is going be put here :)
+function getData(cb) { //cb stands for call back
 
-var xhr = new XMLHttpRequest(); // a built in object that allows us to consume API's
-                                //give us the method to open -send-close- connections
-var data;
+    var xhr = new XMLHttpRequest(); // a built in object that allows us to consume API's
+    //give us the method to open -send-close- connections
+
+    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+
+    xhr.send();
+
+    xhr.onreadystatechange = function () {
+        //console.log(this.readyState); shows the states 1,2,3,4
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText)); //fourth video
+
+
+        }
+    };
+
+}
+
+getData(function (data) {
+    console.log(data);
+});
+
+
+
+
+
+
+/*setTimeout(function(){ // waits for the states to be called until is stores the result in data
+    console.log(data);
+}, 500); */
 
 /* xhr.onreadystatechange = function(){
     if(this.readyState == 4 && this.status == 200){
@@ -16,23 +44,6 @@ var data;
     }
 }; */
 
-function setData(jsonData){
+/*function setData(jsonData){
     data = jsonData;
-}
-
-xhr.onreadystatechange = function(){
-    //console.log(this.readyState); shows the states 1,2,3,4
-    if(this.readyState == 4 && this.status == 200){
-          data=JSON.parse(this.responseText); //fourth video
-    }
-};
-
-setTimeout(function(){ // waits for the states to be called until is stores the result in data
-    console.log(data);
-}, 500);
-
-
-
-xhr.open("GET","https://ci-swapi.herokuapp.com/api/");
-
-xhr.send();
+}*/
